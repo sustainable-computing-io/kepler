4ec9d1d1c8bf20126d85bd353003083e3d8bd87b
Huamin Chen <hchen@redhat.com>
2022-04-27 14:53:53 -0400

    add nvml gpu energy accounting

---
+++ pkg/power/gpu/gpu_nvml.go
@@ -0,0 +1,86 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package gpu
+
+import (
+	"fmt"
+
+	"github.com/NVIDIA/go-nvml/pkg/nvml"
+)
+
+var (
+	devices []nvml.Device
+)
+
+type pidMem struct {
+	pid uint32
+	mem uint64
+}
+
+func Init() error {
+	if ret := nvml.Init(); ret != nvml.SUCCESS {
+		return fmt.Errorf("failed to init nvml: %v", nvml.ErrorString(ret))
+	}
+	count, ret := nvml.DeviceGetCount()
+	if ret != nvml.SUCCESS {
+		nvml.Shutdown()
+		return fmt.Errorf("failed to get nvml device count: %v", nvml.ErrorString(ret))
+	}
+	devices = make([]nvml.Device, count)
+	for i := 0; i < count; i++ {
+		device, ret := nvml.DeviceGetHandleByIndex(i)
+		if ret != nvml.SUCCESS {
+			nvml.Shutdown()
+			return fmt.Errorf("failed to get nvml device %d: %v ", i, nvml.ErrorString(ret))
+		}
+		devices[i] = device
+	}
+	return nil
+}
+
+func Shutdown() bool {
+	return nvml.Shutdown() == nvml.SUCCESS
+}
+
+func GetEnergyPerPid() (map[uint32]float64, error) {
+	m := make(map[uint32]float64)
+
+	for device := range devices {
+		power, ret := device.GetPowerUsage()
+		if ret != nvml.SUCCESS {
+			fmt.Printf("failed to get power usage on device %v: %v\n", device, nvml.ErrorString(ret))
+			continue
+		}
+		pids, ret := device.GetComputeRunningProcesses()
+		if ret != nvml.SUCCESS {
+			fmt.Printf("failed to get compute processes on device %v: %v", device, nvml.ErrorString(ret))
+			continue
+		}
+		totalMem := uint64(0)
+		pm := make([]pidMem, len(pids))
+		// get used memory of each pid
+		for i, pid := range pids {
+			pm[i].pid = pid.Pid
+			pm[i].mem = pid.UsagedGpuMemory
+			totalMem += pm[i].mem
+		}
+		// use per pid used memory/total used memory to estimate per pid energy
+		for _, p := range pm {
+			m[p.pid] = float64(power * p.mem / totalMem)
+		}
+	}
+	return m, nil
+}
