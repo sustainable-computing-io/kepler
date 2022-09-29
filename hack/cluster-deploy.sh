b427d6af72d67273bbdd087356f723c453ee1c9a
Sam Yuan <yy19902439@126.com>
2022-09-29 23:05:37 +0800

    CI:update integration test with deployment check and push action enhancement
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- hack/cluster-deploy.sh
+++ hack/cluster-deploy.sh
@@ -31,10 +31,29 @@ function main() {
 
     # Ignore errors because some clusters might not have prometheus operator
     kubectl apply -f ${MANIFESTS_OUT_DIR} || true
-    kubectl rollout status daemonset kepler-exporter -n kepler --timeout 60s
-
-    echo "Check the logs of the kepler-exporter with:"
-    echo "kubectl -n kepler logs daemonset.apps/kepler-exporter"
+    
+    # round for 3 times and each for 60s
+    # check if the rollout status is running
+    deploy_status=1
+    for i in 1 2 3
+    do
+        echo "check deployment status for round $i"
+        kubectl rollout status daemonset kepler-exporter -n kepler --timeout 60s
+        #check rollout status
+        if [ $? -eq 0 ]
+        then
+            deploy_status=0
+            break
+        fi
+    done 
+    # if deployment in error
+    if test $[deploy_status] -eq 1
+    then
+        echo "Check the status of the kepler-exporter"
+        kubectl -n kepler describe daemonset.apps/kepler-exporter
+        echo "Check the logs of the kepler-exporter"
+        kubectl -n kepler logs daemonset.apps/kepler-exporter
+    fi
 }
 
 main "$@"
