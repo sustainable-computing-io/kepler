20718a3799c68a6abfd2814ce8855f15219304fc
Sam Yuan <yy19902439@126.com>
2022-10-11 22:44:49 +0800

    impl for integration test
    Signed-off-by: Sam Yuan <yy19902439@126.com>

---
+++ e2e/healthcheck_test.go
@@ -0,0 +1,30 @@
+package e2e_test
+
+import (
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("healthz check should pass", func() {
+	Context("start server with health check", func() {
+		It("should work properly", func() {
+			address, ok := os.LookupEnv("kepler_address")
+			if !ok {
+				address = "localhost:8888"
+				cmd := exec.Command(keplerBin)
+				keplerSession, err = gexec.Start(cmd, nil, nil)
+				Expect(err).NotTo(HaveOccurred())
+				time.Sleep(5 * time.Second) // wait for server start up
+			}
+			resp, err := http.Get("http://" + address + "/healthz")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+		})
+	})
+})
