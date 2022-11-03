9a1aaa519fb63b2826799412bfeb3a4c4109e615
Sam Yuan <yy19902439@126.com>
2022-11-03 20:35:03 +0800

    Test: fix for #350 impl for endpoints test case
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- e2e/e2e_suite_test.go
+++ e2e/e2e_suite_test.go
@@ -1,7 +1,9 @@
 package e2e_test
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -10,9 +12,10 @@ import (
 )
 
 var (
-	tmpDir, keplerBin string
-	keplerSession     *gexec.Session
-	err               error
+	tmpDir, keplerBin, address string
+	keplerSession              *gexec.Session
+	err                        error
+	ok                         bool
 )
 
 func TestE2eTest(t *testing.T) {
@@ -21,19 +24,28 @@ func TestE2eTest(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	tmpDir, err = os.MkdirTemp("", "test-kepler")
-	Expect(err).NotTo(HaveOccurred())
-	keplerBin, err = gexec.Build("../cmd/exporter.go")
-	Expect(err).NotTo(HaveOccurred())
-})
-
-var _ = AfterEach(func() {
-	if keplerSession != nil {
-		keplerSession.Kill()
+	address, ok = os.LookupEnv("kepler_address")
+	if !ok {
+		tmpDir, err = os.MkdirTemp("", "test-kepler")
+		Expect(err).NotTo(HaveOccurred())
+		keplerBin, err = gexec.Build("../cmd/exporter.go")
+		Expect(err).NotTo(HaveOccurred())
+		address = "localhost:8888"
+		cmd := exec.Command(keplerBin)
+		keplerSession, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(func() string {
+			sdtErr := string(keplerSession.Err.Contents())
+			fmt.Println("keplerSession sdtErr", sdtErr)
+			return sdtErr
+		}, timeout, poolingInterval).Should(Or(ContainSubstring("Started Kepler"), ContainSubstring("exiting...")))
 	}
 })
 
 var _ = AfterSuite(func() {
+	if keplerSession != nil {
+		keplerSession.Kill()
+	}
 	os.RemoveAll(tmpDir)
 	os.RemoveAll(keplerBin)
 })
