e96e0bf93618d5c56e834470896f4e30a27061ac
jichenjc <jichenjc@cn.ibm.com>
2023-02-10 03:06:23 +0000

    fix: remove power_data, power_model csv file usage

--- doc/dev/prepare_dev_env.sh
+++ doc/dev/prepare_dev_env.sh
@@ -6,6 +6,4 @@ DATAPATH="/var/lib/kepler/data/"
 mkdir -p ${DATAPATH}
 
 cp ../data/normalized_cpu_arch.csv ${DATAPATH}
-cp ../data/power_data.csv          ${DATAPATH}
-cp ../data/power_model.csv         ${DATAPATH}
 
