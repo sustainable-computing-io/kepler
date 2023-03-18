f1679820bd2ed38eba7954f28a77a83ac0815c92
Sam Yuan <yy19902439@126.com>
2023-03-18 20:15:36 +0800

    fix up
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- hack/installBCC.sh
+++ hack/installBCC.sh
@@ -5,6 +5,11 @@ if [ $CPU_ARCH == "amd64" ]; then
     echo "install bcc for amd64"
     yum install -y http://mirror.centos.org/centos/8-stream/AppStream/x86_64/os/Packages/bcc-0.24.0-2.el8.x86_64.rpm
     yum install -y http://mirror.centos.org/centos/8-stream/PowerTools/x86_64/os/Packages/bcc-devel-0.24.0-2.el8.x86_64.rpm
+    echo "for amd64 install GPU"
+    dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
+    dnf clean all
+    dnf -y module install nvidia-driver:latest-dkms
+    dnf -y install cuda
 fi
 
 if [ $CPU_ARCH == "s390x" ]; then 
@@ -17,4 +22,9 @@ if [ $CPU_ARCH == "arm64" ]; then
     echo "install bcc for arm64"
 	yum install -y https://rpmfind.net/linux/centos-stream/9-stream/AppStream/aarch64/os/Packages/bcc-0.24.0-4.el9.aarch64.rpm
     yum install -y https://rpmfind.net/linux/centos-stream/9-stream/CRB/aarch64/os/Packages/bcc-devel-0.24.0-4.el9.aarch64.rpm
+    echo "for amd64 install GPU"
+    dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/ppc64le/cuda-rhel8.repo
+    dnf clean all
+    dnf -y module install nvidia-driver:latest-dkms
+    dnf -y install cuda
 fi
