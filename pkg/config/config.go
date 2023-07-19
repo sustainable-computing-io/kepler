60eb099123b014c334a6684d8dba69fc974a0459
Yasumasa Suenaga <yasuenag@gmail.com>
2023-07-19 22:25:40 +0900

    Respects configuration from environment variables (#800)
    Signed-off-by: Yasumasa Suenaga <suenaga@oss.nttdata.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -164,14 +164,14 @@ func getIntConfig(configKey string, defaultInt int) int {
 func getConfig(configKey, defaultValue string) (result string) {
 	result = string([]byte(defaultValue))
 	key := string([]byte(configKey))
-	configFile := filepath.Join(configPath, key)
-	value, err := os.ReadFile(configFile)
-	if err == nil {
-		result = bytes.NewBuffer(value).String()
+	strValue, present := os.LookupEnv(key)
+	if present {
+		result = strValue
 	} else {
-		strValue, present := os.LookupEnv(key)
-		if present {
-			result = strValue
+		configFile := filepath.Join(configPath, key)
+		value, err := os.ReadFile(configFile)
+		if err == nil {
+			result = bytes.NewBuffer(value).String()
 		}
 	}
 	return
