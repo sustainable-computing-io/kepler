d992e08b571b6ed599f5d4c28d051ea81c818eba
Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>
2024-03-20 10:33:44 +0100

    remove unsupported online trainer (#1309)
    Signed-off-by: Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>

--- hack/build-manifest.sh
+++ hack/build-manifest.sh
@@ -45,7 +45,6 @@ declare ESTIMATOR_SIDECAR_DEPLOY=false
 declare CI_DEPLOY=false
 declare DEBUG_DEPLOY=false
 declare MODEL_SERVER_DEPLOY=false
-declare TRAINER_DEPLOY=false
 declare QAT_DEPLOY=false
 declare PROMETHEUS_DEPLOY=false
 declare HIGH_GRANULARITY=false
@@ -175,18 +174,6 @@ deploy_model_server() {
 		uncomment_patch openshift "${MANIFESTS_OUT_DIR}"/model-server/kustomization.yaml
 	}
 	ok "Model server deployment configured"
-}
-deploy_trainer() {
-	header "Trainer Deployment"
-	$TRAINER_DEPLOY || {
-		skip "skipping trainer deployment"
-		return 0
-	}
-	uncomment_patch trainer "${MANIFESTS_OUT_DIR}"/model-server/kustomization.yaml
-	$OPENSHIFT_DEPLOY && {
-		uncomment_patch train-ocp "${MANIFESTS_OUT_DIR}"/model-server/kustomization.yaml
-	}
-	ok "Trainer deployment configured"
 }
 deploy_qat() {
 	header "QAT Deployment"
