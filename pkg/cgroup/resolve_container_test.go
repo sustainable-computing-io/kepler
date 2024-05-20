73f8d499642d8d9482d81337e283bb24c3f83b3e
Dave Tucker <dave@dtucker.co.uk>
2024-05-20 16:38:40 +0100

    feat(cgroup): Add podman support
    This commit simplifies the containerID from Cgroup path parsing logic.
    All container IDs from all supported runtimes (including podman) are
    64 character alphanumeric strings.
    
    As such we can simply strip the `.scope` suffix and take the last 64
    characters.
    
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- pkg/cgroup/resolve_container_test.go
+++ pkg/cgroup/resolve_container_test.go
@@ -121,6 +121,7 @@ func TestGetAliveContainers(t *testing.T) {
 		})
 	}
 }
+
 func TestExtractPodContainerIDfromPathWithCgroup(t *testing.T) {
 	tests := []struct {
 		name           string
@@ -131,52 +132,62 @@ func TestExtractPodContainerIDfromPathWithCgroup(t *testing.T) {
 		{
 			name:           "valid path with cgroup 1",
 			path:           "1:name=systemd:/kubepods.slice/kubepods-burstable.slice/kubepods-burstable-podd0511cd2_29d2_4215_be0f_f77bc0609d99.slice/crio-f93ee491b8ed2680d5a909eb098b14a9430173b57ca1c4efedd8768566d67e8e.scope",
-			cgroup:         1,
 			expectedResult: "f93ee491b8ed2680d5a909eb098b14a9430173b57ca1c4efedd8768566d67e8e",
 		},
 		{
 			name:           "valid path with cgroup 2",
 			path:           "0::/kubepods.slice/kubepods-burstable.slice/kubepods-burstable-pod2c9f8a79_5391_454b_88cb_86190881cb96.slice/crio-a09343ca97901516c25036e2b954421254f8c68b384b536064e8999f0c4ed18d.scope",
-			cgroup:         2,
 			expectedResult: "a09343ca97901516c25036e2b954421254f8c68b384b536064e8999f0c4ed18d",
 		},
 		{
 			name:           "valid path with cgroup 1",
 			path:           "13:hugetlb:/system.slice/docker-2fa3e04b676df750842faf017052dd37ea0cc5bc7259a3487a1718c7fe100c94.scope",
-			cgroup:         1,
 			expectedResult: "2fa3e04b676df750842faf017052dd37ea0cc5bc7259a3487a1718c7fe100c94",
 		},
 		{
 			name:           "valid path with cgroup kubelet",
 			path:           "kubelet/kubepods/besteffort/podbdd4097d-6795-404e-9bd8-6a1383386198/c79788e0da15a6597263eb2b9c51d05dd1a9a1d08c53c1161dc8c45d2dac6b38",
-			cgroup:         1,
 			expectedResult: "c79788e0da15a6597263eb2b9c51d05dd1a9a1d08c53c1161dc8c45d2dac6b38",
 		},
 		{
 			name:           "valid path with cgroup systemd",
 			path:           "/sys/fs/cgroup/systemd/system.slice/containerd.service/kubepods-burstable-poda3b200c9_db51_40b4_9d2d_53f8fdf80d7f.slice:cri-containerd:286b15051ec43375190802e1f40562536980a8fd97e75bb89c7f2eec6f995f17",
-			cgroup:         1,
 			expectedResult: "286b15051ec43375190802e1f40562536980a8fd97e75bb89c7f2eec6f995f17",
 		},
 		{
 			name:           "valid path with cgroup 13 and memory",
 			path:           "13:memory:/system.slice/containerd.service/kubepods-besteffort-pod0043435f_1854_4327_b76b_730f681a781d.slice:cri-containerd:01fd96f7ad292b02a8317cde4ecb8c7ef3cc06ffdd113f13410e0837eb2b2a20",
-			cgroup:         1,
 			expectedResult: "01fd96f7ad292b02a8317cde4ecb8c7ef3cc06ffdd113f13410e0837eb2b2a20",
 		},
 		{
 			name:           "valid path with cgroup 11 and blkio",
 			path:           "11:blkio:/kubepods/burstable/podf6adb0af-0855-4bab-b25b-c853f18d0ce2/35b97177dada20362ab90d90ac63cd54e8a41cf87bea34f270631b6da17f4a93",
-			cgroup:         1,
 			expectedResult: "35b97177dada20362ab90d90ac63cd54e8a41cf87bea34f270631b6da17f4a93",
 		},
-
+		{
+			name:           "podman rootless container",
+			path:           "0::/user.slice/user-1000.slice/user@1000.service/user.slice/libpod-3f05ee050f82c0145f1d88c94269c39dff0f07dbf8bba20aafd54b3a75dcaecc.scope/container",
+			expectedResult: "3f05ee050f82c0145f1d88c94269c39dff0f07dbf8bba20aafd54b3a75dcaecc",
+		},
+		{
+			name:           "podman rootful container",
+			path:           "0::/machine.slice/libpod-06dc5f321aad8726aa26559f16ec203bc099245bc44894b14a89fc02b022d1d5.scope/container",
+			expectedResult: "06dc5f321aad8726aa26559f16ec203bc099245bc44894b14a89fc02b022d1d5",
+		},
+		{
+			name:           "podman quadlet",
+			path:           "0::/system.slice/kepler.service/libpod-payload-8e363eb2287da4ccc9f52ffc5de11252ac5fe707e3ddb917a3c0bdf9bb64165b",
+			expectedResult: "8e363eb2287da4ccc9f52ffc5de11252ac5fe707e3ddb917a3c0bdf9bb64165b",
+		},
 		// Add more test cases as needed
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			result, _ := extractPodContainerIDfromPathWithCgroup(test.path, test.cgroup)
+			result, err := extractPodContainerIDfromPathWithCgroup(test.path)
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
 			if result != test.expectedResult {
 				t.Errorf("Expected result: %s, but got: %s", test.expectedResult, result)
 			}
