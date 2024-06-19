8931d616a4772accc7887a482bee1eee0c846d83
Sunil Thaha <sthaha@redhat.com>
2024-06-19 14:31:00 +1000

    feat(validator): load validations from validations.yaml
    This commit is rewrite of existing validator. Changes include
    
    * remove queries.json in favour of validations.yaml
    * move prometheus job names - metal and vm under `prometheus` field
    * Simplify parsing queries making it testable
    * Add gen-report command to regenerate reports
    * Add logging that can be controlled by `log_level` in validator.yaml
    * Fix use of hard-coded ssh-key to generate VM specs
    * Fixes tests
    
    Signed-off-by: Sunil Thaha <sthaha@redhat.com>

--- e2e/tools/validator/src/validator/stresser/__init__.py
+++ e2e/tools/validator/src/validator/stresser/__init__.py
@@ -1,20 +1,31 @@
+import logging
+
 import paramiko
-from  validator import config
+from validator import config
 from typing import NamedTuple
 from datetime import datetime
 
 
+logger = logging.getLogger(__name__)
+
 
 class ScriptResult(NamedTuple):
-    start_time : datetime
-    end_time : datetime
+    start_time: datetime
+    end_time: datetime
+
+
+class RunResult(NamedTuple):
+    stdout: str
+    stderr: str
+    exit_code: int
+
 
 class Remote:
     def __init__(self, config: config.Remote):
         self.host = config.host
         self.pkey = config.pkey
         self.user = config.user
-        self.port =config.port
+        self.port = config.port
         self.password = config.password
 
         self.ssh_client = paramiko.SSHClient()
@@ -24,54 +35,78 @@ class Remote:
         return f"<Remote {self.user}@{self.host}>"
 
     def connect(self):
-        print(f"connecting -> {self.user}@{self.host}")
+        logger.info(f"connecting -> {self.user}@{self.host}")
 
-        if self.pkey is not None:
+        if self.pkey:
+            logger.debug("using pkey to connect")
             pkey = paramiko.RSAKey.from_private_key_file(self.pkey)
             self.ssh_client.connect(
-                hostname=self.host, port=self.port,
-                username=self.user, pkey=pkey,
+                hostname=self.host,
+                port=self.port,
+                username=self.user,
+                pkey=pkey,
             )
         else:
+            logger.debug("using user/password to connect")
             self.ssh_client.connect(
-                hostname=self.host, port=self.port,
-                username=self.user, password=self.password)
+                hostname=self.host,
+                port=self.port,
+                username=self.user,
+                password=self.password,
+            )
 
     def copy(self, script_path, target_script):
         sftp_client = self.ssh_client.open_sftp()
-        print(f"copying script {script_path} to remote - {target_script}")
+        logger.info(f"copying script {script_path} to remote - {target_script}")
         sftp_client.put(script_path, target_script)
         sftp_client.close()
         self.ssh_client.exec_command(f"chmod +x {target_script}")
-        print(f"copying script {script_path} to remote - {target_script} - successfull")
+        logger.info(f"copying script {script_path} to remote - {target_script} - successfull")
 
-    def run_script(self, script_path):
+    def run_script(self, script_path: str) -> ScriptResult:
         self.connect()
-        print(f"Running stress test ...")
+        logger.info("Running script %s ...", script_path)
 
         target_script = "/tmp/stress.sh"
         self.copy(script_path, target_script)
 
         start_time = datetime.now()
         _, stdout, stderr = self.ssh_client.exec_command(target_script)
+
+        print("stdout output:")
+        for line in stdout:
+            print(" ┊ ", line.strip())
+
         exit_status = stdout.channel.recv_exit_status()
         end_time = datetime.now()
         self.ssh_client.close()
 
-        if exit_status == 0:
-            print("script execution successful")
-        else:
-            print("script execution failed")
-
-        print("logs for stress test:")
-        for line in stdout:
-            print("  ", line.strip())
-
-        print("stderr output:")
+        # report any stderr if there is after stdout
+        print("\nstderr output:")
         for line in stderr:
-            print(" ", line.strip())
+            print(" ┊ ", line.strip())
+        print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n\n")
+
+        if exit_status != 0:
+            logger.warn("script execution failed")
+        else:
+            logger.info("script execution successful")
 
         return ScriptResult(
             start_time=start_time,
             end_time=end_time,
+        )
+
+    def run(self, cmd: str, *args) -> RunResult:
+        self.connect()
+        logger.info("running command: %s ", " ".join([cmd, *args]))
+
+        _, stdout, stderr = self.ssh_client.exec_command(" ".join([cmd, *args]))
+        exit_status = stdout.channel.recv_exit_status()
+        self.ssh_client.close()
+
+        return RunResult(
+            stdout=stdout.read().decode("ascii").strip("\n"),
+            stderr=stderr.read().decode("ascii").strip("\n"),
+            exit_code=exit_status,
         )
---
+++ e2e/tools/validator/src/validator/validations/__init__.py
@@ -0,0 +1,88 @@
+import logging
+from typing import Dict, NamedTuple, List
+from validator import config
+import re
+import yaml
+
+
+logger = logging.getLogger(__name__)
+
+
+class QueryTemplate:
+    def __init__(self, query: str, vars: Dict[str, str]) -> None:
+        self._original = query
+        self._vars = vars
+        self._promql = query.format(**vars)
+
+    @property
+    def original(self) -> str:
+        return self._original
+
+    @property
+    def promql(self) -> str:
+        return self._promql
+
+    @property
+    def one_line(self) -> str:
+        q = re.sub(r"\s+", " ", self._promql)
+        q = re.sub(r"\n", "", q)
+        return q
+
+    @property
+    def metric_name(self) -> str:
+        metric = re.search(r"kepler_[a-z_]+_total", self._promql)
+        if metric is None:
+            return f"unknown_{hash(self._promql)}"
+
+        return metric.group(0)
+
+    @property
+    def mode(self) -> str:
+        m = re.search(r"mode=['\"]([a-z]+)['\"]", self._promql)
+        if not m:
+            return "unknown"
+        return m.group(1)
+
+
+class Validation(NamedTuple):
+    name: str
+    expected: QueryTemplate
+    actual: QueryTemplate
+
+
+def read_validations(file_path: str, vars: Dict[str, str]) -> list[Validation]:
+    with open(file_path, "r") as file:
+        yml = yaml.safe_load(file)
+        return [
+            Validation(
+                name=v["name"],
+                expected=QueryTemplate(v["expected"], vars),
+                actual=QueryTemplate(v["actual"], vars),
+            )
+            for v in yml["validations"]
+        ]
+
+
+class Loader:
+    def __init__(self, cfg: config.Validator):
+        self.cfg = cfg
+
+    def load(self) -> List[Validation]:
+        vars = {}
+
+        vm = self.cfg.metal.vm
+        if vm.pid != 0:
+            vars["level"] = "process"
+            vars["vm_selector"] = f'pid="{vm.pid}"'
+        else:
+            vars["level"] = "vm"
+            vars["vm_selector"] = f'vm_id=~".*{vm.name}"'
+
+        prom = self.cfg.prometheus
+        vars["rate_interval"] = prom.rate_interval
+        vars["metal_job_name"] = prom.job.metal
+        vars["vm_job_name"] = prom.job.vm
+
+        logger.debug("vars: %s", vars)
+
+        return read_validations(self.cfg.validations_file, vars)
