ac02eab953696d1dadee802e98411ccfa55c0a52
Maryam Tahhan <mtahhan@redhat.com>
2024-09-04 09:03:14 -0400

    chore: add explanation to bpf-generate.sh
    Signed-off-by: Maryam Tahhan <mtahhan@redhat.com>

--- hack/bpf-generate.sh
+++ hack/bpf-generate.sh
@@ -4,6 +4,18 @@ PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 PKG_BPF="$PROJECT_ROOT/pkg/bpf/"
 PKG_BPFTEST="$PROJECT_ROOT/pkg/bpftest/"
 
+# The goal of this file is to automate the generation of
+# pkg/bpf/mockbpf.go and pkg/bpftest/mocktest.go.
+# When the real *_bpfe[lb].go files are generated they are
+# copied to these mock files with their build flags modified
+# and their go embed directive removed.
+# The idea is that when the real *_bpfe[lb].go exist the mock
+# files are excluded using a go tag. When the *_bpfe[lb].go
+# are cleaned up, the mock files become a placeholder for the
+# implementation so that golanglintci is happy.
+# OPEN: MUST RUN `make clean` before committing mock files to
+# the repo.
+
 generate() {
 # Copy the generated file to a new file
 cp kepler_bpfeb.go mockbpf.go
@@ -35,7 +47,7 @@ gofmt -w mocktest.go
 }
 
 clean() {
-# Remove generated build tags when the real bpf2go files are generate
+# Remove generated build tags in mock files when the real bpf2go files are cleaned up.
 sed -i '/^\/\/go:build mockbpf/d' "$PKG_BPF/mockbpf.go"
 sed -i '/^\/\/ +build mockbpf/d' "$PKG_BPF/mockbpf.go"
 sed -i '/^\/\/go:build mockbpf/d' "$PKG_BPFTEST/mocktest.go"
