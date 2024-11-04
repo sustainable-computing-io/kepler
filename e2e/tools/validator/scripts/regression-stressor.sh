5fa802845436b8c552599edbf870db3ce1dadf2d
vprashar2929 <vibhu.sharma2929@gmail.com>
2024-11-04 16:50:18 +0530

    feat(validator): add support to validate essential metrics produced by Kepler
    This commit introduces functionality to validate essential metrics produced by Kepler
    The following comparisons are included:
    
    - Node Exporter Comparison
      - Validates `node_rapl_<package|core|dram>` metrics against `kepler_node_<package|core|dram>{dev}`
    
    - Kepler Process Comparison
      - Compares `kepler_process_<package|core|dram|platform|other|uncore>{latest}` metrics to
      `kepler_process_<package|core|dram|platform|other|uncore>{dev}`
    
    - Kepler Node Comparison
      - Validates `kepler_node_<package|core|dram|platform|other|uncore>{latest}` against
      `kepler_node_<package|core|dram|platform|other|uncore>{dev}`
    
    Additionally, a stressor script has been added to include system load,
    allowing for real-time validation of Kepler under stress conditions.
    
    Signed-off-by: vprashar2929 <vibhu.sharma2929@gmail.com>

---
+++ e2e/tools/validator/scripts/regression-stressor.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+trap exit_all INT
+exit_all() {
+	pkill -P $$
+}
+
+run() {
+	echo "❯ $*"
+	"$@"
+	echo "      ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"
+}
+
+main() {
+
+	local cpus
+	cpus=$(nproc)
+
+	# load and time
+	local -a load_curve=(
+		0:5
+		10:20
+		25:20
+		50:20
+		75:20
+		50:20
+		25:20
+		10:20
+		0:5
+	)
+	# sleep 5  so that first run and the second run look the same
+	echo "Warmup .."
+	run stress-ng --cpu "$cpus" --cpu-method ackermann --cpu-load 0 --timeout 5
+
+	for x in "${load_curve[@]}"; do
+		local load="${x%%:*}"
+		local time="${x##*:}s"
+		run stress-ng --cpu "$cpus" --cpu-method ackermann --cpu-load "$load" --timeout "$time"
+	done
+}
+
+main "$@"
